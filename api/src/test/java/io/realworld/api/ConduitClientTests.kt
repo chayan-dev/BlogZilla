37d0ce3654429416f16994777bc3d641403dc8d1
Chayan Das <chayandas0308@gmail.com>
2022-03-22 19:11:44 +0530

    Initial commit

---
+++ api/src/test/java/io/realworld/api/ConduitClientTests.kt
@@ -0,0 +1,17 @@
+package io.realworld.api
+
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+
+class ConduitClientTests {
+
+    private val conduitClient=ConduitClient()
+
+    @Test
+    fun `GET articles`(){
+        val articles= conduitClient.api.getArticles().execute()
+        assertNotNull(articles.body()?.articles)
+
+    }
+
+}
